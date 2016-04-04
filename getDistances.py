2f08b3f5b67df3f5ffff2935c4524c1abb69a3a8
Shawn <shawntaylor@weecology.org>
2016-04-04 11:00:54 -0400

    this general method for getting distances seems to be the fastest even though it uses loops

--- getDistances.py
+++ getDistances.py
@@ -11,25 +11,12 @@ class_locations=[]
 max_classes=0
 for this_class in classes:
     class_locations.append(np.argwhere(np.abs(image-this_class)==0))
-    if class_locations[-1].shape[0]>max_classes:
-        max_classes=class_locations[-1].shape[0]
-
-locations=np.zeros((max_classes, 2, len(classes)))
-locations.fill(-99999)
-
-for i, array in enumerate(class_locations):
-    locations[0:array.shape[0],:,i]=array
-
-
-all_locations=np.argwhere(image > -1)
-x=np.min((((all_locations[:,0]-locations[:,0,:]**2)+((all_locations[:,1]-locations[:,1,:])**2))**0.5), 1)
-print(x.shape)
-exit()
 
 pixel=0
 for row in range(image.shape[0]):
     for col in range(image.shape[1]):
-        for
-        print(pixel)
-        pixel+=1
-        x=np.min((((row-locations[:,0,:])**2)+((col-locations[:,1,:])**2))**0.5, 0)  #distance array to single point toCheck
+        for i, this_class in enumerate(classes):
+            print(pixel)
+            pixel+=1
+            locations=class_locations[i]
+            x=np.min((((row-locations[:,0])**2)+((col-locations[:,1])**2))**0.5)  #distance array to single point toCheck
