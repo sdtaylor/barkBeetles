b274e2a3c9dd497adb98c21e456f3c650a0de955
Shawn <shawntaylor@weecology.org>
2016-04-09 12:34:14 -0400

    added western montana data. split 2 areas into testing and training

--- cutToStudyArea.R
+++ cutToStudyArea.R
@@ -1,20 +1,23 @@
-#Take the original exports of the full western US and cut them to the study area in north central coloardo.
+#Take the original exports of the full western US and cut them to the study areas in north central coloardo and western montana. 
 #also add together the rasters for lodgepole pine and ponderosa pine to get total tree deaths/pixel
 library(raster)
 library(rgdal)
 library(sp)
 
 rasterLocation='~/data/barkbeetle/exported_tifs/'
-newLocation='~/data/barkbeetle/study_area_tifs/'
 
 fileList=list.files(rasterLocation, pattern='*tif', full.names = TRUE)
 
 fileList=fileList[!grepl('xml', fileList)]
 fileList=fileList[!grepl('ovr', fileList)]
 
-#studyAreaOutline=readShapePoly('/home/shawn/projects/barkBeetle/gisData/studyAreaOutline.shp', proj4string = CRS(utm_Z12_CRS))
-studyAreaOutline=readOGR('/home/shawn/projects/barkBeetle/gisData', 'studyAreaOutline')
+#studyAreaOutline=readOGR('/home/shawn/projects/barkBeetle/gisData', 'northernColoradoOutline')
+#studyAreaOutline=spTransform(studyAreaOutline, CRS('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0 '))
+#newLocation='./data/trainingArea/'
+
+studyAreaOutline=readOGR('/home/shawn/projects/barkBeetle/gisData', 'westernMontanaOutline')
 studyAreaOutline=spTransform(studyAreaOutline, CRS('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0 '))
+newLocation='./data/testingArea/'
 
 #Rasters have -9999 values for NA that need to be dealt with before adding together the different tree species.
 addRasters=function(x,y){
